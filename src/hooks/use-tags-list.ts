12f3ebe67fc50c16dc114df6062f9bedffc67b99
Lokesh Devnani <contact@lokeshd.com>
2023-03-17 18:56:30 +0530

    Gatsby v5 migration + upgrade other modules

--- src/hooks/use-tags-list.ts
+++ src/hooks/use-tags-list.ts
@@ -11,20 +11,16 @@ interface TagsQueryResult {
 
 const useTagsList = () => {
   const { allMarkdownRemark } = useStaticQuery<TagsQueryResult>(
-    graphql`
-      query TagsListQuery {
-        allMarkdownRemark(
-          filter: {
-            frontmatter: { template: { eq: "post" }, draft: { ne: true } }
-          }
-        ) {
-          group(field: frontmatter___tags) {
-            fieldValue
-            totalCount
-          }
-        }
-      }
-    `,
+    graphql`query TagsListQuery {
+  allMarkdownRemark(
+    filter: {frontmatter: {template: {eq: "post"}, draft: {ne: true}}}
+  ) {
+    group(field: {frontmatter: {tags: SELECT}}) {
+      fieldValue
+      totalCount
+    }
+  }
+}`,
   );
 
   return allMarkdownRemark.group || [];
