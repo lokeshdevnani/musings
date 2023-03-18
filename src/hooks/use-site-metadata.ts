172c034b9ce4e43d65e59fbb89af501c36342f54
Lokesh Devnani <contact@lokeshd.com>
2023-03-19 02:09:14 +0530

    Fix font + fix resumeUrl

--- src/hooks/use-site-metadata.ts
+++ src/hooks/use-site-metadata.ts
@@ -10,6 +10,7 @@ const useSiteMetadata = () => {
               bio
               name
               photo
+              resumeUrl
               contacts {
                 rss
                 line
